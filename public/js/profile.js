bbe7376bf83f61f5a39ed37dc17f8fc8a68cd554
Christine Matthews <candreola@gmail.com>
2024-09-01 16:23:41 -0500

    add handlebars and js code

---
+++ public/js/profile.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const blogTitle = document.querySelector('#blog-title').value.trim();
+    const blogContent = document.querySelector('#blog-content').value.trim();
+
+  
+    if (blogTitle && blogContent ) {
+      const response = await fetch(`/api/blogposts`, {
+        method: 'POST',
+        body: JSON.stringify({ blogTitle, blogContent }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogposts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blog-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blog-post-list')
+    .addEventListener('click', delButtonHandler);
+  
