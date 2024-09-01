51a4f40af8099a11344889d829c9f0ddf1e1e651
Christine Matthews <candreola@gmail.com>
2024-09-01 14:48:33 -0500

    add routes

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
